5c6d93a7366eab3f42ab973158bfccb1f790fea6
Gabriela Villagran <villagranfurg@gmail.com>
2024-03-05 17:22:45 -0300

    atualizando js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 
-function clickmenu(){
-  if (itens.style.display == "block"){
-    itens.style.display = "none"
+let menu = document.querySelector(".menu__hamburger")
+let menuItems = document.querySelector(".lista-menu")
+
+menu.addEventListener("click", clicar)
+
+function clicar(){
+  if (menuItems.style.display == "block"){
+    menuItems.style.display = "none"
   } else {
-    itens.style.display = "block"
+    menuItems.style.display = "block"
   }
 }
